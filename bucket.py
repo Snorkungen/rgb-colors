7d3d1370ef59dd294a74cd4b12b2edf35af71a2c
snorkungen <teuvo2004@gmail.com>
2023-09-11 12:15:26 +0300

    logical fallacies are awesome

--- bucket.py
+++ bucket.py
@@ -6,35 +6,44 @@ filename = path.join("data", "colors-with-contrasting-color-test.csv")
 
 thing = dict()
 
+
+contrasting_colors = list()
+
+COLOR_OF_INTREST = "#2020ff"
+
+
 with open(filename, "r") as file:
     reader = csv.reader(file)
     next(reader)
 
-    pgbar = Progressbar(1)
+    rows = list (reader)
+    pgbar = Progressbar(len(rows),prefix="")
 
-    for row in reader:
+
+    for row in rows:
         hex, rlum, light, clum = row
         thing[hex] = {"rlum": rlum, "light": light, "clum": clum, "lst": list()}
         break
 
     for key in thing:
         d = thing[key]
-        pgbar.prefix = f"computing {key}"
-        pgbar.update(0)
+        dl = d["light"]
+
+        # pgbar.update(0)
+
+        
         for hex, rlum, light, clum in reader:
-            pgbar.count += 1
             pgbar.increment()
 
 
             # TODO THE Logic below is broken
-            if not bool(light) and float(rlum) > float(d["clum"]):
-                pass
-            if bool(light) and float(rlum) < float(d["clum"]):
-                pass
-            else:
+            if dl == "False" and float(rlum) < float(d["clum"]):
+                continue
+            if dl == "True" and float(rlum) > float(d["clum"]):                
                 continue
 
             thing[key]["lst"].append(hex)
+        pgbar.end()
 
 
 with open(path.join("data", "thing.yaml?"), "w") as file:
